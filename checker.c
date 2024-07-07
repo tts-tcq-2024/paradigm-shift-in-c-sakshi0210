2e4e88998b8838ad512d30351201992229bec1ef
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-07-07 18:05:21 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool checkMeasure(const char* measureName, float measureValue, float lowerLimit, float upperLimit) {
-    bool status = true;
-    if (measureValue < lowerLimit) {
+bool isValueInRange(const char* measureName, float value, float lowerLimit, float upperLimit) {
+    if (value < lowerLimit) {
         printf("%s is too low!\n", measureName);
-        status = false;
-    } else if (measureValue > upperLimit) {
-        printf("%s is too high!\n", measureName);
-        status = false;
+        return false;
     }
-    return status;
+    if (value > upperLimit) {
+        printf("%s is too high!\n", measureName);
+        return false;
+    }
+    return true;
 }
 
 bool checkTemperature(float temperature) {
-    return checkMeasure("Temperature", temperature, 0, 45);
+    return isValueInRange("Temperature", temperature, 0, 45);
 }
 
 bool checkSoc(float soc) {
-    return checkMeasure("State of Charge", soc, 20, 80);
+    return isValueInRange("State of Charge", soc, 20, 80);
 }
 
 bool checkChargeRate(float chargeRate) {
-    return checkMeasure("Charge Rate", chargeRate, 0, 0.8f);
+    return isValueInRange("Charge Rate", chargeRate, 0, 0.8f);
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-    return checkTemperature(temperature) && checkSoc(soc) && checkChargeRate(chargeRate);
+    bool tempOk = checkTemperature(temperature);
+    bool socOk = checkSoc(soc);
+    bool chargeOk = checkChargeRate(chargeRate);
+    return tempOk && socOk && chargeOk;
 }
 
 int main() {
     // Test cases
-    if (batteryIsOk(25, 70, 0.7f) != true) return 1;
-    if (batteryIsOk(50, 85, 0.0f) != false) return 1;
-    if (batteryIsOk(-1, 70, 0.7f) != false) return 1;
-    if (batteryIsOk(25, 10, 0.7f) != false) return 1;
-    if (batteryIsOk(25, 70, 0.9f) != false) return 1;
+    if (!batteryIsOk(25, 70, 0.7f)) return 1;
+    if (batteryIsOk(50, 85, 0.0f)) return 1;
+    if (batteryIsOk(-1, 70, 0.7f)) return 1;
+    if (batteryIsOk(25, 10, 0.7f)) return 1;
+    if (batteryIsOk(25, 70, 0.9f)) return 1;
     printf("Some more tests needed\n");
     return 0;
 }
-
-
