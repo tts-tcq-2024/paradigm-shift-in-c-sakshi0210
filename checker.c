49f87978f5954bcc8d7c759e1a7cce6415b2919e
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-07-07 18:21:55 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,40 +1,44 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool checkLowLimit(const char* measureName, float measureValue, float lowerLimit) {
+bool isTooLow(const char* measureName, float measureValue, float lowerLimit) {
     if (measureValue < lowerLimit) {
         printf("%s is too low!\n", measureName);
-        return false;
+        return true;
     }
-    return true;
+    return false;
 }
 
-bool checkHighLimit(const char* measureName, float measureValue, float upperLimit) {
+bool isTooHigh(const char* measureName, float measureValue, float upperLimit) {
     if (measureValue > upperLimit) {
         printf("%s is too high!\n", measureName);
-        return false;
+        return true;
     }
-    return true;
+    return false;
 }
 
-bool checkMeasure(const char* measureName, float measureValue, float lowerLimit, float upperLimit) {
-    return checkLowLimit(measureName, measureValue, lowerLimit) &&
-           checkHighLimit(measureName, measureValue, upperLimit);
+bool checkTemperature(float temperature) {
+    return !isTooLow("Temperature", temperature, 0) && !isTooHigh("Temperature", temperature, 45);
+}
+
+bool checkSoc(float soc) {
+    return !isTooLow("State of Charge", soc, 20) && !isTooHigh("State of Charge", soc, 80);
+}
+
+bool checkChargeRate(float chargeRate) {
+    return !isTooLow("Charge Rate", chargeRate, 0) && !isTooHigh("Charge Rate", chargeRate, 0.8f);
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-    bool tempStatus = checkMeasure("Temperature", temperature, 0, 45);
-    bool socStatus = checkMeasure("State of Charge", soc, 20, 80);
-    bool chargeStatus = checkMeasure("Charge Rate", chargeRate, 0, 0.8f);
-    return tempStatus && socStatus && chargeStatus;
+    return checkTemperature(temperature) && checkSoc(soc) && checkChargeRate(chargeRate);
 }
 
 void runTests() {
-    if (batteryIsOk(25, 70, 0.7f) != true) printf("Test 1 failed\n");
-    if (batteryIsOk(50, 85, 0.0f) != false) printf("Test 2 failed\n");
-    if (batteryIsOk(-1, 70, 0.7f) != false) printf("Test 3 failed\n");
-    if (batteryIsOk(25, 10, 0.7f) != false) printf("Test 4 failed\n");
-    if (batteryIsOk(25, 70, 0.9f) != false) printf("Test 5 failed\n");
+    if (!batteryIsOk(25, 70, 0.7f)) printf("Test 1 failed\n");
+    if (batteryIsOk(50, 85, 0.0f)) printf("Test 2 failed\n");
+    if (batteryIsOk(-1, 70, 0.7f)) printf("Test 3 failed\n");
+    if (batteryIsOk(25, 10, 0.7f)) printf("Test 4 failed\n");
+    if (batteryIsOk(25, 70, 0.9f)) printf("Test 5 failed\n");
     printf("Some more tests needed\n");
 }
 
